3b633e0520c4b60593f5de08f971710b24fcb5fc
snow-zxr <c.snow0908@gmail.com>
2021-11-21 09:54:10 +0000

    code

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,13 +8,13 @@ import Entry from '$lib/Entry.svelte';
 <!-- Entries -->
 <section class="container px-4 py-3">
     <div class="d-flex justify-content-between">
-   	 <div class="p-2">Mood Log</div>
+   	 <div class="p-2 font-monospace">Mood Log</div>
    	 <input class="btn btn-light mb-2" type="button" value="+ New Entry" />
     </div>
 
     <div class="list-group mb-3">
    	 <!-- Individual Entries -->
     	<Entry />
-   	 
+        
     </div>
 </section>
