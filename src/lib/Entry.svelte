7d11b99eb35d0619720086253e903ee1ff38998a
snow-zxr <c.snow0908@gmail.com>
2021-11-21 10:02:58 +0000

    entry

--- src/lib/Entry.svelte
+++ src/lib/Entry.svelte
@@ -5,3 +5,35 @@
 	</div>
 	<small class="text-muted">Comments on medication, activities, etc.</small>
 </button>
+
+<button type="button" class="list-group-item list-group-item-action">
+<div class="d-flex w-100 justify-content-between lh-1">
+    	<h6 class="my-0">2 Nov 2021</h6>
+    	<span class="text-info">Good</span>
+	</div>
+	<small class="text-muted">Comments on medication, activities, etc.</small>
+</button>
+
+<button type="button" class="list-group-item list-group-item-action">	
+	<div class="d-flex w-100 justify-content-between lh-1">
+    	<h6 class="my-0">3 Nov 2021</h6>
+    	<span class="text-primary">Okay</span>
+	</div>
+	<small class="text-muted">Comments on medication, activities, etc.</small>
+</button>
+
+<button type="button" class="list-group-item list-group-item-action">
+	<div class="d-flex w-100 justify-content-between lh-1">
+    	<h6 class="my-0">4 Nov 2021</h6>
+    	<span class="text-warning">Bad</span>
+	</div>
+	<small class="text-muted">Comments on medication, activities, etc.</small>
+</button>
+
+<button type="button" class="list-group-item list-group-item-action">
+	<div class="d-flex w-100 justify-content-between lh-1">
+    	<h6 class="my-0">5 Nov 2021</h6>
+    	<span class="text-danger">Worst</span>
+	</div>
+	<small class="text-muted">Comments on medication, activities, etc.</small>
+</button>
