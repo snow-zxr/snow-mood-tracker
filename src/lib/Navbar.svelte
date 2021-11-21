ae125b06c42913d098cfe9882122a8b60495b0d9
snow-zxr <c.snow0908@gmail.com>
2021-11-21 09:36:33 +0000

    code

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,7 @@
+<nav class="navbar navbar-expand-xl navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">My Mood Tracker🙂</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarDark" aria-controls="navbarDark" aria-expanded="false" aria-label="Toggle navigation">
+    </button>
+  </div>
+</nav>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+import Greeting from '$lib/Greeting.svelte';
+import Entry from '$lib/Entry.svelte';
+</script>
+
+<Greeting />
+
+<!-- Entries -->
+<section class="container px-4 py-3">
+    <div class="d-flex justify-content-between">
+   	 <div class="p-2">Mood Log</div>
+   	 <input class="btn btn-light mb-2" type="button" value="+ New Entry" />
+    </div>
+
+    <div class="list-group mb-3">
+   	 <!-- Individual Entries -->
+    	<Entry />
+   	 
+    </div>
+</section>
