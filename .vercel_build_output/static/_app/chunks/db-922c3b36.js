69a079cc0bfe3170a4073691b233a610c5b301ae
snow-zxr <c.snow0908@gmail.com>
2021-12-12 09:42:40 +0000

    build

---
+++ .vercel_build_output/static/_app/chunks/db-922c3b36.js
@@ -0,0 +1,1 @@
+import{M as s}from"./vendor-9fc60af8.js";const I=s("https://zdquzuzxfuvdlkenpsbq.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzODY5NDU3MiwiZXhwIjoxOTU0MjcwNTcyfQ.3w3Zuvmg1AQlzs-XmW7plUD35Hstk_JPceCOTK21LYk");export{I as s};
